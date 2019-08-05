1fdff2b2a2ba7dd5fbadaedbc5eff91373722277
Yang Li <37635679+YangLiyli131@users.noreply.github.com>
2019-08-05 10:55:21 -0400

    Add files via upload

---
+++ Algorithms/Sorting/Running Time of Algorithms.java
@@ -0,0 +1,57 @@
+// problem link
+// https://www.hackerrank.com/challenges/runningtime/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the runningTime function below.
+    static int runningTime(int[] arr) {
+        int res = 0;
+        for(int p = 1; p < arr.length; p++){
+            int temp = arr[p];
+            int i = p;
+            while(i > 0 && arr[i-1] > temp){
+                arr[i] = arr[i-1];
+                i--;
+                res++;
+            }
+            arr[i] = temp;
+        }
+        return res;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = runningTime(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
